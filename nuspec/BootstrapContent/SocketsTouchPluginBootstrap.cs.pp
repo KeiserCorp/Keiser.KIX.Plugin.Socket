bd2432ff1a138024994c9f49ec4419c3f90788d4
Zach Cardoza <bayssmekanique@live.com>
2014-11-20 13:02:32 -0800

    Adds NuGet Support

---
+++ nuspec/BootstrapContent/SocketsTouchPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+    public class SocketsPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Keiser.Mvx.Plugins.Sockets.PluginLoader, Keiser.Mvx.Plugins.Sockets>
+    {
+    }
+}
